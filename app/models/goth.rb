f353e0a08acd4fda75e5e5be4e3c3d3a7efb02d6
Steven Wilkin <stevenwilkin@gmail.com>
2009-01-05 21:20:49 +0000

    can now "publish" a goth

--- app/models/goth.rb
+++ app/models/goth.rb
@@ -5,4 +5,15 @@ class Goth < ActiveRecord::Base
 		self.find(:first, :order => 'RAND()')
 	end
 
+	# set the published_at datetime of a goth to current timestamp
+	# goth will be chosen at random if there are any left which haven't been published
+	# otherwise republish the oldest published goth
+	def self.publish
+		g = self.find(:first, :conditions => {:published_at => nil}, :order => 'RAND()')
+		unless g
+			g = self.find(:first, :order => 'published_at ASC')
+		end
+		g.update_attribute(:published_at, Time.now)
+	end
+
 end
